7a039cc07e913fd4b80b6b6ca13d756904cd5b1a
adityajha <aj12102003@gmail.com>
2025-04-18 19:08:34 +0530

    First Push

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <div className="flex items-center text-red-900 justify-center h-screen bg-gray-100 text-3xl font-semibold">
+      Hello, world!
+    </div>
+  );
+};
+
+export default App;
